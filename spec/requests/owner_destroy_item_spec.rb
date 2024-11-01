3cb6ec60c71f3c12f0830ec43c9530fc7400af99
Bruno-H-Terto <herculanoterto@gmail.com>
2024-11-01 01:58:30 -0300

    Testes de request para os cenários de edição de um item

---
+++ spec/requests/owner_destroy_item_spec.rb
@@ -0,0 +1,104 @@
+require 'rails_helper'
+
+describe 'Proprietário deleta um item' do
+  context 'DELETE /take_away_stores/:take_away_store_id/dishes/:id' do
+    it 'deve estar autenticado' do
+      owner = Owner.create!(name: 'Harry', surname: 'Potter', register_number: '402.793.150-58',
+          email: 'quadribol@email.com', password: 'treina_dev13')
+      store = owner.create_take_away_store!(trade_name: 'Grifinória', corporate_name: 'Hogwarts LTDA',
+          register_number: '76.898.265/0001-10', phone_number: '(11) 98800-0000', street: 'Beco diagonal',
+          number: '13', district: 'Bolsão', city: 'Hogsmeade', state: 'SP', zip_code: '11000-000', complement: '',
+          email: 'potter@email.com')
+      BusinessHour.day_of_weeks.each do |key, _|
+        store.business_hours.create!(day_of_week: key, status: :open, open_time: '09:00',
+            close_time: '17:00')
+      end
+      dish = store.items.create!(name: 'Pizza', description: 'Quatro queijos', calories: 120, type: 'Dish')
+
+      delete take_away_store_dish_path(store, dish)
+
+      expect(Dish.count).to eq 1
+      expect(Dish.last).to eq dish
+    end
+
+    it 'não pode excluir de outro proprietário' do
+      owner = Owner.create!(name: 'Harry', surname: 'Potter', register_number: '402.793.150-58',
+          email: 'quadribol@email.com', password: 'treina_dev13')
+      store = owner.create_take_away_store!(trade_name: 'Grifinória', corporate_name: 'Hogwarts LTDA',
+          register_number: '76.898.265/0001-10', phone_number: '(11) 98800-0000', street: 'Beco diagonal',
+          number: '13', district: 'Bolsão', city: 'Hogsmeade', state: 'SP', zip_code: '11000-000', complement: '',
+          email: 'potter@email.com')
+      BusinessHour.day_of_weeks.each do |key, _|
+        store.business_hours.create!(day_of_week: key, status: :open, open_time: '09:00',
+            close_time: '17:00')
+      end
+      dish = store.items.create!(name: 'Pizza', description: 'Quatro queijos', calories: 120, type: 'Dish')
+      other_owner = Owner.create!(name: 'Jhon', surname: 'Doe', register_number: '307.331.850-02',
+          email: 'jhon@email.com', password: 'treina_dev13')
+      other_store = other_owner.create_take_away_store!(trade_name: 'Pastelaria Top', corporate_name: 'Pastel LTDA',
+          register_number: '92.303.111/0001-95', phone_number: '(11) 88888-0000', street: 'Av. Getúlio Vargas',
+          number: '91', district: 'Centro', city: 'São Paulo', state: 'SP', zip_code: '12000-000', complement: 'Loja 1',
+          email: 'pateltop@email.com')
+      BusinessHour.day_of_weeks.each do |key, _|
+        other_store.business_hours.create!(day_of_week: key, status: :open, open_time: '10:00',
+            close_time: '18:00')
+      end
+
+      login_as other_owner, scope: :owner
+      delete take_away_store_dish_path(store, dish)
+
+      expect(Dish.count).to eq 1
+      expect(Dish.last).to eq dish
+    end
+  end
+  context 'DELETE /take_away_stores/:take_away_store_id/beverages/:id' do
+    it 'deve estar autenticado' do
+      owner = Owner.create!(name: 'Harry', surname: 'Potter', register_number: '402.793.150-58',
+          email: 'quadribol@email.com', password: 'treina_dev13')
+      store = owner.create_take_away_store!(trade_name: 'Grifinória', corporate_name: 'Hogwarts LTDA',
+          register_number: '76.898.265/0001-10', phone_number: '(11) 98800-0000', street: 'Beco diagonal',
+          number: '13', district: 'Bolsão', city: 'Hogsmeade', state: 'SP', zip_code: '11000-000', complement: '',
+          email: 'potter@email.com')
+      BusinessHour.day_of_weeks.each do |key, _|
+        store.business_hours.create!(day_of_week: key, status: :open, open_time: '09:00',
+            close_time: '17:00')
+      end
+      beverage = store.items.create!(name: 'Coca-Cola', description: '2l', calories: 90, type: 'Beverage')
+
+      delete take_away_store_beverage_path(store, beverage)
+
+      expect(Beverage.count).to eq 1
+      expect(Beverage.last).to eq beverage
+    end
+
+    it 'não pode excluir de outro proprietário' do
+      owner = Owner.create!(name: 'Harry', surname: 'Potter', register_number: '402.793.150-58',
+          email: 'quadribol@email.com', password: 'treina_dev13')
+      store = owner.create_take_away_store!(trade_name: 'Grifinória', corporate_name: 'Hogwarts LTDA',
+          register_number: '76.898.265/0001-10', phone_number: '(11) 98800-0000', street: 'Beco diagonal',
+          number: '13', district: 'Bolsão', city: 'Hogsmeade', state: 'SP', zip_code: '11000-000', complement: '',
+          email: 'potter@email.com')
+      BusinessHour.day_of_weeks.each do |key, _|
+        store.business_hours.create!(day_of_week: key, status: :open, open_time: '09:00',
+            close_time: '17:00')
+      end
+      beverage = store.items.create!(name: 'Coca-Cola', description: '2l', calories: 90, type: 'Beverage')
+      other_owner = Owner.create!(name: 'Jhon', surname: 'Doe', register_number: '307.331.850-02',
+          email: 'jhon@email.com', password: 'treina_dev13')
+      other_store = other_owner.create_take_away_store!(trade_name: 'Pastelaria Top', corporate_name: 'Pastel LTDA',
+          register_number: '92.303.111/0001-95', phone_number: '(11) 88888-0000', street: 'Av. Getúlio Vargas',
+          number: '91', district: 'Centro', city: 'São Paulo', state: 'SP', zip_code: '12000-000', complement: 'Loja 1',
+          email: 'pateltop@email.com')
+      BusinessHour.day_of_weeks.each do |key, _|
+        other_store.business_hours.create!(day_of_week: key, status: :open, open_time: '10:00',
+            close_time: '18:00')
+      end
+
+      login_as other_owner, scope: :owner
+      delete take_away_store_beverage_path(store, beverage)
+
+      expect(Beverage.count).to eq 1
+      expect(Beverage.last).to eq beverage
+    end
+  end
+end
---
+++ spec/requests/owner_edit_a_item_spec.rb
@@ -0,0 +1,109 @@
+require 'rails_helper'
+
+describe 'Proprietário edita um item' do
+  context 'PATCH /take_away_stores/:take_away_store_id/dishes/:id' do
+    it 'deve estar autenticado' do
+      owner = Owner.create!(name: 'Harry', surname: 'Potter', register_number: '402.793.150-58',
+          email: 'quadribol@email.com', password: 'treina_dev13')
+      store = owner.create_take_away_store!(trade_name: 'Grifinória', corporate_name: 'Hogwarts LTDA',
+          register_number: '76.898.265/0001-10', phone_number: '(11) 98800-0000', street: 'Beco diagonal',
+          number: '13', district: 'Bolsão', city: 'Hogsmeade', state: 'SP', zip_code: '11000-000', complement: '',
+          email: 'potter@email.com')
+      BusinessHour.day_of_weeks.each do |key, _|
+        store.business_hours.create!(day_of_week: key, status: :open, open_time: '09:00',
+            close_time: '17:00')
+      end
+      dish = store.items.create!(name: 'Pizza', description: 'Quatro queijos', calories: 120, type: 'Dish')
+
+      patch take_away_store_dish_path(store, dish), params: { dish: { name: 'Hamburguer'} }
+
+      expect(response).to redirect_to(new_owner_session_path)
+      expect(dish.name).not_to eq 'Hamburguer'
+      expect(dish.name).to eq 'Pizza'
+    end
+
+    it 'somente para o próprio estabelecimento' do
+      owner = Owner.create!(name: 'Harry', surname: 'Potter', register_number: '402.793.150-58',
+          email: 'quadribol@email.com', password: 'treina_dev13')
+      store = owner.create_take_away_store!(trade_name: 'Grifinória', corporate_name: 'Hogwarts LTDA',
+          register_number: '76.898.265/0001-10', phone_number: '(11) 98800-0000', street: 'Beco diagonal',
+          number: '13', district: 'Bolsão', city: 'Hogsmeade', state: 'SP', zip_code: '11000-000', complement: '',
+          email: 'potter@email.com')
+      BusinessHour.day_of_weeks.each do |key, _|
+        store.business_hours.create!(day_of_week: key, status: :open, open_time: '09:00',
+            close_time: '17:00')
+      end
+      dish = store.items.create!(name: 'Pizza', description: 'Quatro queijos', calories: 120, type: 'Dish')
+      other_owner = Owner.create!(name: 'Jhon', surname: 'Doe', register_number: '307.331.850-02',
+          email: 'jhon@email.com', password: 'treina_dev13')
+      other_store = other_owner.create_take_away_store!(trade_name: 'Pastelaria Top', corporate_name: 'Pastel LTDA',
+          register_number: '92.303.111/0001-95', phone_number: '(11) 88888-0000', street: 'Av. Getúlio Vargas',
+          number: '91', district: 'Centro', city: 'São Paulo', state: 'SP', zip_code: '12000-000', complement: 'Loja 1',
+          email: 'pateltop@email.com')
+      BusinessHour.day_of_weeks.each do |key, _|
+        other_store.business_hours.create!(day_of_week: key, status: :open, open_time: '10:00',
+            close_time: '18:00')
+      end
+
+      login_as other_owner, scope: :owner
+      patch take_away_store_dish_path(store, dish), params: { dish: { name: 'Hamburguer'} }
+
+      expect(response).to redirect_to(take_away_store_path(other_store))
+      expect(dish.name).not_to eq 'Hamburguer'
+      expect(dish.name).to eq 'Pizza'
+    end
+  end
+
+  context 'PATCH /take_away_stores/:take_away_store_id/beverages/:id' do
+    it 'deve estar autenticado' do
+      owner = Owner.create!(name: 'Harry', surname: 'Potter', register_number: '402.793.150-58',
+          email: 'quadribol@email.com', password: 'treina_dev13')
+      store = owner.create_take_away_store!(trade_name: 'Grifinória', corporate_name: 'Hogwarts LTDA',
+          register_number: '76.898.265/0001-10', phone_number: '(11) 98800-0000', street: 'Beco diagonal',
+          number: '13', district: 'Bolsão', city: 'Hogsmeade', state: 'SP', zip_code: '11000-000', complement: '',
+          email: 'potter@email.com')
+      BusinessHour.day_of_weeks.each do |key, _|
+        store.business_hours.create!(day_of_week: key, status: :open, open_time: '09:00',
+            close_time: '17:00')
+      end
+      drink = store.items.create!(name: 'Coca-Cola', description: '2l', calories: 80, type: 'Beverage')
+
+      patch take_away_store_beverage_path(store, drink), params: { beverage: { name: 'Vinho'} }
+
+      expect(response).to redirect_to(new_owner_session_path)
+      expect(drink.name).not_to eq 'Vinho'
+      expect(drink.name).to eq 'Coca-Cola'
+    end
+
+    it 'somente para o próprio estabelecimento' do
+      owner = Owner.create!(name: 'Harry', surname: 'Potter', register_number: '402.793.150-58',
+          email: 'quadribol@email.com', password: 'treina_dev13')
+      store = owner.create_take_away_store!(trade_name: 'Grifinória', corporate_name: 'Hogwarts LTDA',
+          register_number: '76.898.265/0001-10', phone_number: '(11) 98800-0000', street: 'Beco diagonal',
+          number: '13', district: 'Bolsão', city: 'Hogsmeade', state: 'SP', zip_code: '11000-000', complement: '',
+          email: 'potter@email.com')
+      BusinessHour.day_of_weeks.each do |key, _|
+        store.business_hours.create!(day_of_week: key, status: :open, open_time: '09:00',
+            close_time: '17:00')
+      end
+      drink = store.items.create!(name: 'Coca-Cola', description: '2l', calories: 80, type: 'Beverage')
+      other_owner = Owner.create!(name: 'Jhon', surname: 'Doe', register_number: '307.331.850-02',
+          email: 'jhon@email.com', password: 'treina_dev13')
+      other_store = other_owner.create_take_away_store!(trade_name: 'Pastelaria Top', corporate_name: 'Pastel LTDA',
+          register_number: '92.303.111/0001-95', phone_number: '(11) 88888-0000', street: 'Av. Getúlio Vargas',
+          number: '91', district: 'Centro', city: 'São Paulo', state: 'SP', zip_code: '12000-000', complement: 'Loja 1',
+          email: 'pateltop@email.com')
+      BusinessHour.day_of_weeks.each do |key, _|
+        other_store.business_hours.create!(day_of_week: key, status: :open, open_time: '10:00',
+            close_time: '18:00')
+      end
+
+      login_as other_owner, scope: :owner
+      patch take_away_store_beverage_path(store, drink), params: { beverage: { name: 'Vinho'} }
+
+      expect(response).to redirect_to(take_away_store_path(other_store))
+      expect(drink.name).not_to eq 'Vinho'
+      expect(drink.name).to eq 'Coca-Cola'
+    end
+  end
+end
