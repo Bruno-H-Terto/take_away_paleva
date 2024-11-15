fdd16d82c60db451115a125c73f078370aca08f4
Bruno-H-Terto <herculanoterto@gmail.com>
2024-11-15 07:14:58 -0300

    Concluído função de filtro por status e primeiro cenário de falha ao passar status inexistente

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,12 +1,31 @@
 class Api::V1::OrdersController < Api::V1::ApplicationController
+  before_action :set_take_away_store
   def index
-    store = TakeAwayStore.find_by(code: params[:store_code])
-    raise ActiveRecord::RecordNotFound, 'Estabelecimento não localizado' if store.nil?
-
-    orders = store.orders.as_json(
-      except: [:created_at, :updated_at, :phone_number, :register_number, :email]
-      )
+    orders = sanitizer_response(@store.orders)
 
     render status: 200, json: orders
+  end
+
+  def status
+    orders = sanitizer_response(@store.orders.where(status: params[:status]))
+
+    if orders.empty?
+      orders = sanitizer_response(@store.orders)
+    end
+
+    render status: 200, json: orders
+  end
+
+  private
+
+  def set_take_away_store
+    @store = TakeAwayStore.find_by(code: params[:store_code])
+    raise ActiveRecord::RecordNotFound if @store.nil?
+  end
+
+  def sanitizer_response(object)
+    object.as_json(
+      except: [:created_at, :updated_at, :phone_number, :register_number, :email]
+      )
   end
 end
--- spec/requests/apis/user_filter_list_orders_spec.rb
+++ spec/requests/apis/user_filter_list_orders_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Usuário vê listagem de pedidos de um Estabelecimento a partir de seu código' do
-  context 'GET /api/v1/orders' do
+  context 'GET /api/v1/stores/:store_code/orders' do
     it 'com sucesso' do
       owner = Owner.create!(name: 'Harry', surname: 'Potter', register_number: '402.793.150-58',
           email: 'quadribol@email.com', password: 'treina_dev13')
@@ -41,7 +41,7 @@ describe 'Usuário vê listagem de pedidos de um Estabelecimento a partir de seu
       expect(json_response[3]['code']).to eq order_4.code
     end
 
-    it 'só tem acesso a listagem de um Estabelecimento' do
+    it 'só tem acesso a listagem do Estabelecimento procurado' do
       owner = Owner.create!(name: 'Harry', surname: 'Potter', register_number: '402.793.150-58',
           email: 'quadribol@email.com', password: 'treina_dev13')
       store = owner.create_take_away_store!(trade_name: 'Grifinória', corporate_name: 'Hogwarts LTDA',
@@ -107,6 +107,68 @@ describe 'Usuário vê listagem de pedidos de um Estabelecimento a partir de seu
       json_response = JSON.parse(response.body)
       expect(json_response.keys).to include 'error_message'
       expect(json_response['error_message']).to include 'Ocorreu um erro interno'
+    end
+  end
+
+  context 'GET /api/v1/stores/:store_code/orders/status' do
+    it 'com sucesso' do
+      owner = Owner.create!(name: 'Harry', surname: 'Potter', register_number: '402.793.150-58',
+          email: 'quadribol@email.com', password: 'treina_dev13')
+      store = owner.create_take_away_store!(trade_name: 'Grifinória', corporate_name: 'Hogwarts LTDA',
+          register_number: '76.898.265/0001-10', phone_number: '(11) 98800-0000', street: 'Beco diagonal',
+          number: '13', district: 'Bolsão', city: 'Hogsmeade', state: 'SP', zip_code: '11000-000', complement: '',
+          email: 'potter@email.com')
+      order_1 = store.orders.create!(name: 'Jhon', phone_number: '(11) 999998888', email: 'jhon@email.com', register_number: '701.128.250-52')
+      order_2 = store.orders.create!(name: 'José', phone_number: '(21) 988887777', email: '', register_number: '')
+      order_3 = store.orders.create!(name: 'Maria', phone_number: '', email: 'maria@email.com', register_number: '690.814.440-26')
+      order_3.preparing!
+      order_4 = store.orders.create!(name: 'Ana', phone_number: '', email: 'ana@email.com', register_number: '')
+      order_4.canceled!
+
+      get status_api_v1_store_orders_path(store.code), params: { status: 'waiting_confirmation' }
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+      json_response = JSON.parse(response.body)
+      expect(json_response.length).to eq 2
+      expect(json_response.inspect).to include 'Jhon'
+      expect(json_response.inspect).to include order_1.code
+      expect(json_response.inspect).to include 'José'
+      expect(json_response.inspect).to include order_2.code
+      expect(json_response.inspect).not_to include 'Maria'
+      expect(json_response.inspect).not_to include order_3.code
+      expect(json_response.inspect).not_to include 'Ana'
+      expect(json_response.inspect).not_to include order_4.code
+    end
+
+    it 'informa status inexistente' do
+      owner = Owner.create!(name: 'Harry', surname: 'Potter', register_number: '402.793.150-58',
+          email: 'quadribol@email.com', password: 'treina_dev13')
+      store = owner.create_take_away_store!(trade_name: 'Grifinória', corporate_name: 'Hogwarts LTDA',
+          register_number: '76.898.265/0001-10', phone_number: '(11) 98800-0000', street: 'Beco diagonal',
+          number: '13', district: 'Bolsão', city: 'Hogsmeade', state: 'SP', zip_code: '11000-000', complement: '',
+          email: 'potter@email.com')
+      order_1 = store.orders.create!(name: 'Jhon', phone_number: '(11) 999998888', email: 'jhon@email.com', register_number: '701.128.250-52')
+      order_2 = store.orders.create!(name: 'José', phone_number: '(21) 988887777', email: '', register_number: '')
+      order_3 = store.orders.create!(name: 'Maria', phone_number: '', email: 'maria@email.com', register_number: '690.814.440-26')
+      order_3.preparing!
+      order_4 = store.orders.create!(name: 'Ana', phone_number: '', email: 'ana@email.com', register_number: '')
+      order_4.canceled!
+
+      get status_api_v1_store_orders_path(store.code), params: { status: 'kamehameha' }
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+      json_response = JSON.parse(response.body)
+      expect(json_response.length).to eq 4
+      expect(json_response.inspect).to include 'Jhon'
+      expect(json_response.inspect).to include order_1.code
+      expect(json_response.inspect).to include 'José'
+      expect(json_response.inspect).to include order_2.code
+      expect(json_response.inspect).to include 'Maria'
+      expect(json_response.inspect).to include order_3.code
+      expect(json_response.inspect).to include 'Ana'
+      expect(json_response.inspect).to include order_4.code
     end
   end
 end
